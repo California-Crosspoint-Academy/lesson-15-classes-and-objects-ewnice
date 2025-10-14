f3cbc2dd0cd14c1ee2a1575ce98bdb66171eaf75
ewnice <eunicechan26@crosspointacademy.org>
2025-10-14 18:47:07 +0000

    Notes + Exercise L15

---
+++ Exercise.java
@@ -0,0 +1,76 @@
+//Eunice Chan
+//2025-10-13
+
+//1a. Rectangle
+//b. myRect
+//c. double and int
+
+//2. public Rectangle(double length, int width) 
+
+//3. 
+Lunch yummy5 = new Lunch(true, 900);
+
+//4a. BankAccount
+//b. account39
+//c. double
+
+//5. a class is like a cookie cutter, an object is like a cookie
+
+//6. void means there's nothing to return but constructors cannot have return types; school needs to be capitalized to match the class name
+
+//7. a
+
+//8. a
+
+//9. the object used two integers in the method but the method states you need an integer and boolean; constructor should not be in the main method but in the class  
+
+//10a. int
+//b. int
+//c. double
+
+//11. 
+public BibleStory(String x, int y, double z);
+
+//12. 
+public int var1;
+public double var2;
+public String sss;
+
+//13. 
+public void Samson(double zorro);
+public String getDelilah();
+
+//14. BibleStory philistine = new BibleStory("Ralph", 19, 24.18);
+
+//15. gravy.var2 = 106.9;
+
+//16. System.out.println(bart.sss);
+
+//17. String
+
+//18.
+public class Trail {
+    int x;
+    int y; 
+    String s;
+
+    public Trail(String s) {
+    this.s=s;
+    this.x=10;
+    this.y=10;
+    }
+
+    public String met() {
+        int p = x * y;
+        return Integer.toHexString(p);
+    }
+}
+
+//19. 
+double peach = zippo.peachyDandy(1); 
+
+//20. 127.31 is a double vartype, not an integer as the method calls for an integer only
+
+
+
+
---
+++ Notes.java
@@ -0,0 +1,14 @@
+//Eunice Chab
+//2025-10-13
+
+//Declare class 
+
+//Constructor: same name as class, initialize an object  
+
+//Signature: declare a method  
+
+//Method: performs an action 
+
+//State Variable: declared within class but outside method, constructor, or method; set value for an object at specific time 
+
+//Instantiation: create objects  
